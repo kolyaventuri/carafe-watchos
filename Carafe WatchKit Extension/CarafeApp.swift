bfcaace34b62e354d80d6f89c82273b7804ea669
Kolya Venturi <kolya.venturi@enx3s.com>
2021-06-04 12:11:09 -0600

    Initial Commit

---
+++ Carafe WatchKit Extension/CarafeApp.swift
@@ -0,0 +1,19 @@
+//
+//  CarafeApp.swift
+//  Carafe WatchKit Extension
+//
+//  Created by Kolya Venturi on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct CarafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
